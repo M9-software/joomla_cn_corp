d0a471aa3eae3df4ae7247887011b8e51a55919c
radision <radision@126.com>
2019-03-17 07:54:05 +0800

    k2

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-03-16 12:03:51 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-03-16T12:03:51+00:00	INFO 144.34.185.218	update	Update started by user Super User (674). Old version is 3.9.3.
+2019-03-16T12:03:55+00:00	INFO 144.34.185.218	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.4-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20190316%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20190316T120348Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=9d7da7f0809c24ef1cd624613633e4c10f719fa19aefbea9027d980ec8836c45.
+2019-03-16T12:04:44+00:00	INFO 144.34.185.218	update	File Joomla_3.9.4-Stable-Update_Package.zip downloaded.
+2019-03-16T12:04:45+00:00	INFO 144.34.185.218	update	Starting installation of new version.
+2019-03-16T12:04:54+00:00	INFO 144.34.185.218	update	Finalising installation.
+2019-03-16T12:04:54+00:00	INFO 144.34.185.218	update	Deleting removed files and folders.
+2019-03-16T12:04:56+00:00	INFO 144.34.185.218	update	Cleaning up after installation.
+2019-03-16T12:04:56+00:00	INFO 144.34.185.218	update	Update to version 3.9.4 is complete.
